205a3e5f0939331db2ec685522c392a9ee9c0c34
Rohith Aralikatti <rohith.aralikatti@gmail.com>
2022-09-27 10:26:40 -0400

    Lab5 added.

---
+++ Labs/Lab5_Fragments/app/src/main/java/course/labs/fragmentslab/CounterViewModel.kt
@@ -0,0 +1,58 @@
+package course.labs.fragmentslab
+
+import android.util.Log
+import androidx.lifecycle.*
+
+class CounterViewModel : ViewModel(), DefaultLifecycleObserver {
+
+    // This class defines the ViewModel which keeps track of the number of times onCreate(), onStart() and onResume() have been called.
+    companion object {
+        private const val TAG = "CounterViewModel"
+    }
+
+    // TODO :
+    // Create variables to keep a track of the number of times onCreate(), onStart() and onResume() have been called.
+    // To keep track of each count, define two variables as specified below.
+    // Define a private variable of type MutableLiveData that can only be modified within the ViewModel class.
+    // Define an internal/public variable of type LiveData that can be accessed externally by the UI/fragment but cannot be modified.
+    // Use a backing property to specify the getter function for the internal/public variable
+    // Refer to the link below for a more detailed explanation/example
+    // https://developer.android.com/codelabs/basic-android-kotlin-training-viewmodel#4
+
+
+    init {
+        // TODO:
+        // Set initial value of counts to zero
+
+    }
+
+    internal fun bindToActivityLifecycle(mainActivity: MainActivity) {
+        // TODO :
+        // Add the current instance of CounterViewModel as a LifeCycleObserver to the MainActivity
+        // Use the addObserver function
+
+    }
+
+    override fun onResume(owner: LifecycleOwner) {
+        // TODO :
+        // Update the appropriate count variable
+        Log.i(TAG,"Entered onResume")
+
+    }
+
+    override fun onCreate(owner: LifecycleOwner) {
+        // TODO :
+        // Update the appropriate count variable
+        Log.i(TAG,"Entered onCreate")
+
+    }
+
+    override fun onStart(owner: LifecycleOwner) {
+        // TODO :
+        // Update the appropriate count variable
+        Log.i(TAG,"Entered onStart")
+
+    }
+
+
+}
---
+++ Labs/Lab5_Fragments/app/src/main/java/course/labs/fragmentslab/FirstFragment.kt
@@ -0,0 +1,59 @@
+package course.labs.fragmentslab
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import course.labs.fragmentslab.databinding.FirstFragmentBinding
+
+class FirstFragment : Fragment() {
+
+    /** Binding to XML layout */
+    private lateinit var binding: FirstFragmentBinding
+    // Create a variable of type CounterViewModel to keep track of counts
+    private lateinit var viewModel: CounterViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Use the provided binding object to inflate the layout.
+        binding = FirstFragmentBinding.inflate(inflater, container, false)
+
+        // Update ActionBar label to distinguish which Fragment is displayed
+        (requireActivity() as AppCompatActivity).supportActionBar?.title = this.javaClass.simpleName
+
+        // Set onClickListener to navigate to the second fragmant from the first
+        binding.fab.setOnClickListener {
+            findNavController().navigate(FirstFragmentDirections.actionFirstFragmentToSecondFragment()
+            )
+        }
+        // Return the root view.
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // TODO:
+        // Initialize CounterViewModel instance
+
+        // TODO:
+        // Use binding to display initial counts
+
+        // The function below updates the counts over time
+        beginObservingCounter()
+    }
+
+    private fun beginObservingCounter() {
+
+        // TODO:
+        // Register observers for each of the count variables
+        // In the body of the observe function, update the text to be displayed by using the binding
+
+    }
+}
